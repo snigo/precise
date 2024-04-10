765b6f661763cdaa95ae8bdee3c7ec0db50f63d4
Igor Snitkin <snigo.ogins@gmail.com>
2024-04-10 11:10:12 +0200

    add JSDocs for rounding functions

--- src/round.ts
+++ src/round.ts
@@ -1,60 +1,77 @@
 import type { RoundingFn } from './types.js';
 
-function assertPrecision(precision: number) {
+function _assertPrecision(precision: number) {
   if (precision < -100 || precision > 100) {
     throw RangeError('Precision value should be in -100..100 range.');
   }
 }
 
-function unsignZero(num: number): number {
+function _unsignZero(num: number): number {
   return num === 0 ? 0 : num;
 }
 
-function shiftExp(num: number, shift: number) {
+function _shiftExp(num: number, shift: number) {
   const parts: (string | number)[] = num.toExponential().split('e');
   parts[1] = Number(parts[1]) + shift;
   return Number(parts.join('e'));
 }
 
 /**
- * Rounds number to a certain precision.
- * Negative precision will work as well:
+ * Rounds number to a certain precision
+ * @note Negative precision will work as well
+ * @example
  * ```
- * round(1234, -2) => 1200
+ * round(1.234, 2) // => 1.23
+ * round(1.234, 2, 'ceil') // => 1.24
+ * round(1234, -2) // => 1200
  * ```
+ * @param num - The number to round.
+ * @param precision - The number of decimal places to keep. Defaults to 0.
+ * @param mode - The rounding mode to use. Defaults to 'round'.
+ * @returns The rounded number.
  */
 export function round(
   num: number,
   precision = 0,
   mode: RoundingFn = 'round',
 ): number {
-  assertPrecision(precision);
-  const m = Math[mode];
+  _assertPrecision(precision);
+  const fn = Math[mode];
   if (!precision) {
-    return unsignZero(m(num));
+    return _unsignZero(fn(num));
   }
-  const p = Math.trunc(precision);
-  const w = m(shiftExp(num, p));
-  return unsignZero(shiftExp(w, -p));
+  const p = Math.floor(precision);
+  const w = fn(_shiftExp(num, p));
+  return _unsignZero(_shiftExp(w, -p));
 }
 
 /**
- * Rounds number to a certain precision towards Infinity.
- * Negative precision will work as well:
+ * Rounds number to a certain precision towards Infinity
+ * @note Negative precision will work as well
+ * @example
  * ```
+ * ceil(1.234, 2) // => 1.24
  * ceil(1234, -2) => 1300
  * ```
+ * @param num - The number to round.
+ * @param precision - The number of decimal places to keep. Defaults to 0.
+ * @returns The rounded number.
  */
 export function ceil(num: number, precision = 0): number {
   return round(num, precision, 'ceil');
 }
 
 /**
- * Rounds number to a certain precision towards -Infinity.
- * Negative precision will work as well:
+ * Rounds number to a certain precision towards -Infinity
+ * @note Negative precision will work as well
+ * @example
  * ```
- * round(1234, -2) => 1200
+ * floor(1.234, 2) // => 1.23
+ * floor(1234, -2) => 1200
  * ```
+ * @param num - The number to round.
+ * @param precision - The number of decimal places to keep. Defaults to 0.
+ * @returns The rounded number.
  */
 export function floor(num: number, precision = 0): number {
   return round(num, precision, 'floor');
