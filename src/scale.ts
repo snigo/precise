8302b1521119d612a48e2229cf716a20d0ee777e
Igor Snitkin <snigo.ogins@gmail.com>
2024-04-10 11:16:15 +0200

    add JSDocs for scale functions

--- src/scale.ts
+++ src/scale.ts
@@ -1,8 +1,35 @@
+import { round } from './round.js';
+
 /**
- * Calculates scale of number:
- * the number of digits to the right of the decimal point
+ * Calculates scale of number, the number of digits
+ * to the right of the decimal point
+ * @example
+ * ```
+ * getScale(0.001) // => 3
+ * getScale(1000) // => -3
+ * getScale(0) // => 0
+ * ```
+ * @param num - The number to calculate the scale of.
+ * @returns The scale of the number.
  */
 export function getScale(num: number): number {
   const parts = num.toExponential().split('e');
   return Number(parts[0]?.split('.')[1]?.length ?? 0) - Number(parts[1]);
+}
+
+/**
+ * Calculates the unit of the number, the smallest number
+ * that can be represented by the number within it's precision.
+ * @example
+ * ```
+ * getUnit(12.347) // => 0.001
+ * getUnit(2000) // => 1000
+ * getUnit(0.3) // => 0.1
+ * ```
+ * @param num - The number to calculate the unit of.
+ * @returns The unit of the number.
+ */
+export function getUnit(num: number): number {
+  const scale = getScale(num);
+  return round(10 ** -scale, scale);
 }
